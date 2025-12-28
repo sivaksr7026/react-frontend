549d5b2e7c7d15e56a5d1db5a6aa549e5cbdd15f
Siva <sivaksr7026@gmail.com>
2025-12-29 01:10:45 +0530

    Update registration API endpoint to production URL

--- src/shoppingfolder/pages/Register.js
+++ src/shoppingfolder/pages/Register.js
@@ -19,7 +19,7 @@ const Register = () => {
 
   try {
     const response = await fetch(
-      "http://localhost:5000/teachers/register",
+      "https://backend-nodejs-zeta.vercel.app/register",
       {
         method: "POST",
         headers: { "Content-Type": "application/json" },
